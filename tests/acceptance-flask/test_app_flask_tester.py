459ee9c54b3e37f7b4596a8a796c00d0f1eada9a
Elleta <elleta.mcdaniel@allianz.es>
2022-07-28 21:48:55 +0200

    tests

--- tests/acceptance-flask/test_app_flask_tester.py
+++ tests/acceptance-flask/test_app_flask_tester.py
@@ -21,7 +21,7 @@ class TestApp(unittest.TestCase):
         tester = app.test_client(self)
         print(f"token: {self.token}")
         headers = {"Authorization": f"Bearer {TestApp.token}"}
-        response = tester.get('/api/v1/users/1/products', content_type='application/json', headers=headers)
+        response = tester.get('/api/v1/users/2/products', content_type='application/json', headers=headers)
 
         data= response.json        
         print(f"get_user_products: {data}")
