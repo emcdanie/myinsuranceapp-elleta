a8c0fc43b18b22ea54c7d26edb8e06199ec4e051
Elleta <elleta.mcdaniel@allianz.es>
2022-07-29 10:21:48 +0200

    tests

--- tests/acceptance-flask/test_app_flask_tester.py
+++ tests/acceptance-flask/test_app_flask_tester.py
@@ -1,9 +1,11 @@
 import unittest 
 from project import app 
+import requests 
 
 class TestApp(unittest.TestCase):
 
     token=''
+    base_url='http://localhost:5000/api/v1'
     
     def test_1_getToken(self): 
         tester = app.test_client(self) 
@@ -26,7 +28,6 @@ class TestApp(unittest.TestCase):
         data= response.json        
         print(f"get_user_products: {data}")
         self.assertEqual(response.status_code, 200)
-
         self.assertTrue(len(data)>0)
     
     def test_3_get_user_products_invalid_token(self):
@@ -37,3 +38,17 @@ class TestApp(unittest.TestCase):
         data= response.json 
         print(f"get_user_products: {data}")
         self.assertTrue(response.status_code > 400)
+
+    # def test_4_post_user_products(self):
+    #     url=f"{self.base_url}/users/1/products"
+    #     headers = {"Authorization": f"Bearer {TestApp.token}"}
+    #     product_data= {"id":3}
+    #     response =requests.post(f"{self.base_url}/api/v1/users/2/products", content_type='application/json', headers=headers)
+    #     data= response.json 
+
+    #     print(f"post_user_products: {data}")
+    #     self.assertEqual(data[-1]["id"], product_data["id"])
+    #     self.assertEqual(response.status_code, 200)
+
+     
+       
